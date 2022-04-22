828e1a17f5effc858e6992fa785cb5ef259e4dc4
Amonnuns <ramonwx15@gmail.com>
2022-04-22 18:19:14 -0300

    variables used in main.cpp

---
+++ Nodemcu/include/ConstantDefs.h
@@ -0,0 +1,9 @@
+const char* ssid = "";
+const char* password = "";
+const char* mqtt_server = "";
+const char* mqtt_user = "";
+const char* mqtt_pass= "";
+const char* topic = "";
+#define endereco #youraddresshere
+#define colunas 16
+#define linhas 2
