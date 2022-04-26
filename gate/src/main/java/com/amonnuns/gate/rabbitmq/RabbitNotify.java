01cc23cff951376da27004df1cd8bd5e144c1cb2
Amonnuns <ramonwx15@gmail.com>
2022-04-25 21:22:13 -0300

    Rabbitmq capabilities added to gate module

---
+++ gate/src/main/java/com/amonnuns/gate/rabbitmq/RabbitNotify.java
@@ -0,0 +1,25 @@
+package com.amonnuns.gate.rabbitmq;
+
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RabbitNotify {
+
+    private final RabbitTemplate rabbitTemplate;
+
+    private static final String NOME_EXCHANGE = "notifyNODE";
+    private static final String ROUTING_KEY =  "cerberus.news";
+
+    public RabbitNotify(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+        rabbitTemplate.setExchange(NOME_EXCHANGE);
+        rabbitTemplate.setRoutingKey(ROUTING_KEY);
+    }
+
+    public void enviarNotificacao(String message){
+        rabbitTemplate.send(new Message(message.getBytes()));
+    }
+
+}
