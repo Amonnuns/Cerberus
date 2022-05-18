f3bfee02040dd80d64a71cafe2f97282803c7c45
Amonnuns <ramonwx15@gmail.com>
2022-05-18 20:08:53 -0300

    new variables addition

--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -14,12 +14,23 @@ variable "dns_prefix" {
     default = "k8scerberus"
 }
 
+variable "storage_name" {
+    default = "cerberusrage"
+}
+variable "pool_name" {
+    default = "cerberuspool"
+}
+
+variable "vm_size" {
+    default = "Standard_F4s_v2"
+
+}
 variable "ssh_public_key" {
   default = "../mykey.pub"
 }
 
 variable "agent_count" {
-    default = 2
+    default = 1
 }
 
 variable "aks_service_principal_app_id"{
